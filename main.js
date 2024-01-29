8ff2f7f8281186d6f426671bbcbf5def63929958
MICHAEL KOFI NYANYUIE <mnyanyuie@gmail.com>
2024-01-29 15:35:08 +0100

    Update main.js
    Affiche tous les lignes a la clique d'un bouton

--- main.js
+++ main.js
@@ -1,1 +1,46 @@
+//AFFICHAGE SUR LE SITE
 
+
+//REQUETES XHR
+
+//Défi 1.3 ->
+//Affichage de tous les Lignes
+
+let bouton = document.querySelector(".display")
+let retour = document.querySelector(".retour")
+
+
+function afficherDonnee(lignes){
+    bouton.addEventListener('click', ()=>{
+        if (bouton.style.display === "flex"){
+            bouton.style.display = "none"
+            retour.style.display = "flex"
+        
+            let listWrapper = document.createElement("ol")
+            for (let l = 0; l < lignes.length; l++){
+                let liElement = document.createElement("li")
+                liElement.className = "list"
+                liElement.textContent = lignes[l].id
+                listWrapper.appendChild(liElement)
+            } 
+        } else{
+            bouton.style.display = "flex"
+            retour.style.display = "none"
+        }
+        
+    })
+
+}
+
+// Requete pour avoir tous les Lignes proposés
+const xhr = new XMLHttpRequest()
+xhr.open("GET", "https://api.tisseo.fr/v2/lines.json?key=a3732a1074e2403ce364ad6e71eb998cb")
+xhr.onreadystatechange = function(){
+    if (xhr.status === 200 && xhr.readyState === 4){
+        let reponse = JSON.parse(xhr.responseText)
+        let lignes = reponse.lines.line
+        afficherDonnee(lignes)
+        console.log(lignes)
+    }
+
+}
